9aa9f08e42ce392cde0df617917dda90eab64247
mrsadeghi4 <mrsadeghi4@gmail.com>
2023-04-22 15:31:22 +0330

    add apiv1 task resource

--- anakonda/config/config.py
+++ anakonda/config/config.py
@@ -1,9 +1,11 @@
 from os import environ
 
+
 class Config:
-    ENV = environ.get("ANAKONDA_API_ENV","production")
+    ENV = environ.get("ANAKONDA_API_ENV", "production")
     DEBUG = bool(int(environ.get("ANAKONDA_API_DEBUG", "0")))
     TESTING = DEBUG
     SECRET_KEY = environ.get("ANAKONDA_API_SECRET_KEY", "secretkey")
-    JSONIFY_PRETTYPRINT_REGULAR = bool(int(environ.get("ANAKONDA_API_JSON_PRETTYPRINT", "0")))
-    
+    JSONIFY_PRETTYPRINT_REGULAR = bool(
+        int(environ.get("ANAKONDA_API_JSON_PRETTYPRINT", "0"))
+    )
