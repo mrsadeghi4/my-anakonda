dd09202e7a354cd83fbffe571a93aaf44e6a68c1
mrsadeghi4 <mrsadeghi4@gmail.com>
2023-04-22 10:13:06 +0330

    add apiv1 blueprint

--- anakonda/anakonda.py
+++ anakonda/anakonda.py
@@ -1,8 +1,13 @@
-from flask import Flask
+from flask import Flask, Blueprint
+from flask_restful import Api
+
 from .config import Config
+
+apiv1_bp = Blueprint("apiv1_bp", __name__, url_prefix="/api/v1")
+apiv1 = Api(apiv1_bp)
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
-    print(app.config)
+    app.register_blueprint(apiv1_bp)
     return app
